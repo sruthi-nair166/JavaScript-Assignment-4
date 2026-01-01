4d4a6863f97b37d8f8d4bf8351118f81a910bd3e
Sruthi <sruthivnair78@gmail.com>
2026-01-01 16:44:29 +0530

    add contacts display

--- scripts.js/script.js
+++ scripts.js/script.js
@@ -6,16 +6,73 @@ const enterNameInput = document.getElementById("enter-name");
 const enterNumberInput = document.getElementById("enter-number");
 const cancelBtn = document.getElementById("cancel");
 const saveBtn = document.getElementById("save");
+const contactsList = document.querySelector(".contact-list");
+const emptyList = document.querySelector(".empty-list");
+const addBtn2 = document.getElementById("add-2");
 
-const contactsArray = [];
+let contactsArray = [];
 
 function add(value) {
   addEditModal.classList.remove("hide");
+
+  if (!value) {
+    enterNameInput.value = "";
+    enterNumberInput.value = "";
+    return;
+  }
 
   if (/^\d+$/.test(value)) {
     enterNumberInput.value = value;
   } else {
     enterNameInput.value = value;
+  }
+}
+
+function displayContacts() {
+  if (!contactsArray) {
+    return;
+  }
+
+  emptyList.classList.add("hide");
+
+  contactsArray.forEach((c) => {
+    const contact = document.createElement("button");
+    contact.classList.add("contact");
+
+    const name = document.createElement("p");
+    name.textContent = c.name;
+    contact.appendChild(name);
+
+    const iconWrapper = document.createElement("div");
+
+    const call = document.createElement("button");
+    iconWrapper.appendChild(call);
+
+    const videoCall = document.createElement("button");
+    iconWrapper.appendChild(videoCall);
+
+    contact.appendChild(iconWrapper);
+    contactsList.appendChild(contact);
+  });
+}
+
+async function fetchContacts() {
+  try {
+    const res = await fetch("http://localhost:3000/contacts");
+
+    console.log("STATUS:", res.status);
+
+    if (!res.ok) throw new Error();
+
+    const data = await res.json();
+    console.log("DATA:", data);
+
+    contactsArray = data;
+    console.log(contactsArray);
+    displayContacts();
+    console.log("DISPLAY SUCCESS");
+  } catch (err) {
+    console.error("No Data");
   }
 }
 
@@ -44,6 +101,10 @@ addBtn.addEventListener("click", () => {
   add(input.value.trim());
 });
 
+addBtn2.addEventListener("click", () => {
+  add();
+});
+
 closeBtn.addEventListener("click", () => {
   close();
 });
@@ -67,3 +128,5 @@ saveBtn.addEventListener("click", (e) => {
 
   close();
 });
+
+fetchContacts();
